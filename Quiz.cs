0ba809fa12a7fd58c44957bb5592635fc15b0386
swu1234 <98143038+swu1234@users.noreply.github.com>
2022-02-06 23:04:21 +0900

    지금까지 작성한 스크립트입니다.
    bound, cameramanager : 카메라 영역 설정 관련 스크립트
    movingobject : 플레이어 스크립트
    quiz, quizmanager, quiztest : 팝업퀴즈 스크립트
    startpoint, transfermap : 씬 간 이동 스크립트

---
+++ Quiz.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quiz
+{
+    public string question; // Бъ
+    public string[] answers; // ґд
+}
